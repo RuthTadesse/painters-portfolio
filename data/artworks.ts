e3760ade2e11a833394855ba7fd9bdeb84ab8fd9
unknown <hundienahom@gmail.com>
2025-05-04 23:27:31 +0300

    Addis fly consultancy  poster uplaod

--- data/artworks.ts
+++ data/artworks.ts
@@ -131,4 +131,14 @@ export const artworks = [
     category: "Poster",
     size: '24" x 36"',
   },
+  {
+    id: 13,
+    title: "ADDIS FLY TRAVEL AGENCY",
+    price: 250,
+    description:
+      "For the Tigat Driving School poster, I crafted a dynamic design using a bluescale and yellow palette, with bold typography for the slogan ITS TIME TO DRIVE. My skills ensure the TIGAT logo pops and the call-to-action is clear, driving engagement effectively.",
+    image: "/Addis sky consultancy poster.jpg",
+    category: "Poster",
+    size: '24" x 36"',
+  },
 ];
